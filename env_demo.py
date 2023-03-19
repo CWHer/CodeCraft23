95c086f3c88a5bf81733e94e76c35f6de32535f7
CWHer <cwher@outlook.com>
2023-03-19 16:28:13 +0800

    feat: add env demo

---
+++ env_demo.py
@@ -0,0 +1,33 @@
+from RobotEnv.env_wrapper import EnvWrapper
+import os
+import random
+import argparse
+
+
+if __name__ == "__main__":
+    # test wrapper
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--map-id", default="maps/1.txt")
+    parser.add_argument("--env-binary-name", default="./Robot_fast")
+    parser.add_argument("--env-args", default="-d")
+    parser.add_argument("--env-wrapper-name", default="./env_wrapper")
+    parser.add_argument("--pipe-name", default=f"/tmp/pipe_{random.random()}")
+    args = parser.parse_args()
+    robot_env_path = os.path.join(os.path.dirname(__file__), "RobotEnv")
+    args.map_id = os.path.join(robot_env_path, args.map_id)
+    args.env_binary_name = os.path.join(robot_env_path, args.env_binary_name)
+    args.env_wrapper_name = os.path.join(robot_env_path, args.env_wrapper_name)
+    print(args)
+
+    launch_command = f"{args.env_binary_name} {args.env_args} "\
+        f"-m {args.map_id} \"{args.env_wrapper_name} {args.pipe_name}\""
+    env = EnvWrapper(args.pipe_name, launch_command)
+
+    obs = env.reset()
+    while True:
+        action = ["forward 0 1"]
+        obs, done = env.recv()
+        if done:
+            break
+        env.send(action)
+    env.close()
