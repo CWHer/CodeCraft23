fd56686e3dd82f9b9f5abb36c1882b155f5ce5e7
CWHer <cwher@outlook.com>
2023-03-21 00:43:39 +0800

    fix: update sac network

--- rl_sac/sac_network.py
+++ rl_sac/sac_network.py
@@ -12,6 +12,7 @@ class MLP(nn.Module):
         for hidden_size in hidden_sizes:
             self._net_list.append(nn.LazyLinear(hidden_size))
             self._net_list.append(nn.ReLU(inplace=True))
+        self._net_list.pop(-1)
         self._net = nn.Sequential(*self._net_list)
 
     def forward(self, obs) -> torch.Tensor:
@@ -36,15 +37,17 @@ class ActorNet(nn.Module):
             # obs_embedding_shape: (E)
             # act_embedding_shape: (K', E)
             obs_embedding = self.obs_encoder(obs[k])
+            normed_obs_embedding = obs_embedding / torch.norm(obs_embedding)
             act_embedding = self.action_encoder(act[k])
-            # logits_shape: (K', 1)
-            logits = torch.sum(obs_embedding * act_embedding,
-                               dim=-1, keepdim=True)
+            normed_act_embedding = act_embedding / \
+                torch.norm(act_embedding, dim=-1, keepdim=True)
+            # logits_shape: (K')
+            logits = torch.sum(normed_obs_embedding * normed_act_embedding, dim=-1)
             batch_logits.append(logits)
-        # batch_logits_shape: (B, max_K, 1)
+        # batch_logits_shape: (B, max_K)
         max_dim = max([logits.shape[0] for logits in batch_logits])
         batch_logits = [
-            F.pad(logits, (0, 0, 0, max_dim - logits.shape[0]), value=-1e10)
+            F.pad(logits, (0, max_dim - logits.shape[0]), "constant", -1e10)
             for logits in batch_logits
         ]
         batch_logits = torch.stack(batch_logits, dim=0)
@@ -71,12 +74,26 @@ class CrticNet(nn.Module):
             # obs_embedding_shape: (E)
             # act_embedding_shape: (K', E)
             obs_embedding = self.obs_encoder(obs[k])
-            act_embedding = self.action_encoder(act[k]).mean(dim=0)
-            value = self.critic_mlp(
-                torch.cat([obs_embedding, act_embedding], dim=-1))
+            normed_obs_embedding = obs_embedding / torch.norm(obs_embedding)
+            act_embedding = self.action_encoder(act[k])
+            normed_act_embedding = act_embedding / \
+                torch.norm(act_embedding, dim=-1, keepdim=True)
+            if normed_act_embedding.ndim == 1:
+                normed_act_embedding = normed_act_embedding.unsqueeze(0)
+            value = torch.cat([
+                self.critic_mlp(
+                    torch.cat([normed_obs_embedding, normed_act_embedding[i]], dim=-1))
+                for i in range(normed_act_embedding.shape[0])
+            ], dim=0)
             batch_value.append(value)
 
-        return torch.stack(batch_value, dim=0)
+        max_dim = max([value.shape[0] for value in batch_value])
+        batch_value = [
+            F.pad(value, (0, max_dim - value.shape[0]), "constant", -1e10)
+            for value in batch_value
+        ]
+        batch_value = torch.stack(batch_value, dim=0)
+        return batch_value
 
 
 if __name__ == "__main__":
