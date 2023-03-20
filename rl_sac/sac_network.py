2c4fce0f41d15608af70b6e97ddf0a78a6f70c44
CWHer <cwher@outlook.com>
2023-03-20 19:41:53 +0800

    feat: add rubbish network

--- rl_sac/sac_network.py
+++ rl_sac/sac_network.py
@@ -1,9 +1,12 @@
+from typing import List
+
 import torch
+import torch.nn.functional as F
 from torch import nn
 
 
-class PreprocessNet(nn.Module):
-    def __init__(self, hidden_sizes=[256, 128, 64, 1]) -> None:
+class MLP(nn.Module):
+    def __init__(self, hidden_sizes) -> None:
         super().__init__()
         self._net_list = []
         for hidden_size in hidden_sizes:
@@ -17,29 +20,73 @@ class PreprocessNet(nn.Module):
 
 class ActorNet(nn.Module):
     def __init__(self,
-                 preprocess_net: nn.Module,
-                 action_shape: int,
+                 obs_encoder: nn.Module,
+                 action_encoder: nn.Module,
                  ) -> None:
         super().__init__()
-        self._preprocess = preprocess_net
-        self._last = nn.LazyLinear(action_shape)
+        self.obs_encoder = obs_encoder
+        self.action_encoder = action_encoder
 
-    def forward(self, obs: torch.Tensor) -> torch.Tensor:
-        assert isinstance(obs, torch.Tensor)
-        embeddings = self._preprocess(obs)
-        logits = self._last(embeddings)
-        return logits
+    def forward(self,
+                obs: List[torch.Tensor],
+                act: List[torch.Tensor]
+                ) -> torch.Tensor:
+        batch_logits = []
+        for k in range(len(obs)):
+            # obs_embedding_shape: (E)
+            # act_embedding_shape: (K', E)
+            obs_embedding = self.obs_encoder(obs[k])
+            act_embedding = self.action_encoder(act[k])
+            # logits_shape: (K', 1)
+            logits = torch.sum(obs_embedding * act_embedding,
+                               dim=-1, keepdim=True)
+            batch_logits.append(logits)
+        # batch_logits_shape: (B, max_K, 1)
+        max_dim = max([logits.shape[0] for logits in batch_logits])
+        batch_logits = [
+            F.pad(logits, (0, 0, 0, max_dim - logits.shape[0]), value=-1e10)
+            for logits in batch_logits
+        ]
+        batch_logits = torch.stack(batch_logits, dim=0)
+        return batch_logits
 
 
-class CriticNet(nn.Module):
+class CrticNet(nn.Module):
     def __init__(self,
-                 preprocess_net: nn.Module,
-                 last_size: int = 1) -> None:
+                 obs_encoder: nn.Module,
+                 action_encoder: nn.Module,
+                 critic_mlp: nn.Module,
+                 ) -> None:
         super().__init__()
-        self._preprocess = preprocess_net
-        self._last = nn.LazyLinear(last_size)
+        self.obs_encoder = obs_encoder
+        self.action_encoder = action_encoder
+        self.critic_mlp = critic_mlp
 
-    def forward(self, obs: torch.Tensor):
-        assert isinstance(obs, torch.Tensor)
-        embeddings = self._preprocess(obs)
-        return self._last(embeddings)
+    def forward(self,
+                obs: List[torch.Tensor],
+                act: List[torch.Tensor]
+                ) -> torch.Tensor:
+        batch_value = []
+        for k in range(len(obs)):
+            # obs_embedding_shape: (E)
+            # act_embedding_shape: (K', E)
+            obs_embedding = self.obs_encoder(obs[k])
+            act_embedding = self.action_encoder(act[k]).mean(dim=0)
+            value = self.critic_mlp(
+                torch.cat([obs_embedding, act_embedding], dim=-1))
+            batch_value.append(value)
+
+        return torch.stack(batch_value, dim=0)
+
+
+if __name__ == "__main__":
+    # test
+    obs_encoder = MLP([128, 64])
+    act_encoder = MLP([64, 64])
+    actor_net = ActorNet(obs_encoder, act_encoder)
+    critic_net = CrticNet(obs_encoder, act_encoder, MLP([128, 64, 1]))
+
+    obs = [torch.randn(300), torch.randn(300)]
+    act = [torch.randn(10, 300), torch.randn(20, 300)]
+    print(actor_net(obs, act))
+    print(critic_net(obs, act))
