059d435601509df858e293ae4fb1cc650eaa789c
CWHer <cwher@outlook.com>
2023-03-22 15:43:48 +0800

    feat: add item-based greedy

--- item_centric/scheduler.py
+++ item_centric/scheduler.py
@@ -2,7 +2,11 @@
 import random
 from typing import Any, Dict, List
 
-from task_utils import MetaTask
+import numpy as np
+
+from task_utils import MetaTask, decayFunc
+
+from .task_manager import ItemTaskManager
 
 
 class BaseScheduler:
@@ -33,12 +37,57 @@ class BaseScheduler:
 class GreedyScheduler(BaseScheduler):
     def __init__(self) -> None:
         super().__init__()
+        self.input_money = [3000, 4400, 5800, 15400, 17200, 19200, 76000]
+        self.output_money = [6000, 7600, 9200, 22500, 25000, 27500, 105000]
+        self.delta = np.array(self.output_money) - np.array(self.input_money)
 
     def assign(self,
                obs: Dict[str, Any],
                station_tasks: List[List[MetaTask]],
                assigned_tasks: List[List[MetaTask]]
-               ) -> None:
+               ) -> bool:
         # HACK: FIXME: DO NOT reschedule tasks, we avoid this by scheduling ahead
-        # TODO:
-        raise NotImplementedError()
+        assert len(assigned_tasks) == self.num_robots
+        all_station_tasks = sum(station_tasks, [])
+        idle_indices = [i for i in range(
+            self.num_robots) if not assigned_tasks[i]]
+        if not all_station_tasks \
+                or not idle_indices:
+            return False
+
+        max_efficiency = 0
+        selected_task = None
+        selected_robot_id = 0
+
+        # TODO: sell to non-sink station first
+        for robot_id in idle_indices:
+            robot_loc = np.array([
+                obs["robots"][robot_id]["loc_x"],
+                obs["robots"][robot_id]["loc_y"]
+            ])
+            for task in all_station_tasks:
+                src_loc = np.array([
+                    obs["stations"][task.src_station_id]["loc_x"],
+                    obs["stations"][task.src_station_id]["loc_y"]
+                ])
+                robot_src_time = \
+                    ItemTaskManager.moveTimeEst(robot_loc, src_loc)
+                estimated_total_time = max(
+                    max(robot_src_time, task.src_ready_time.max) +
+                    task.dst_src_time,
+                    task.dst_ready_time.max
+                )
+                delta = self.delta[task.item_type - 1] * \
+                    decayFunc(estimated_total_time, 9000, 0.8)
+                efficiency = delta / estimated_total_time
+                if efficiency > max_efficiency:
+                    max_efficiency = efficiency
+                    selected_task = task
+                    selected_robot_id = robot_id
+
+        assert selected_task is not None
+        selected_task.robot_id = selected_robot_id
+        selected_task.update(obs)
+        assigned_tasks[selected_robot_id].append(selected_task)
+
+        return True
