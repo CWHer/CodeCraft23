6c10339253ad89dba22cb04b23efe7c701adc6e2
CWHer <cwher@outlook.com>
2023-03-26 14:20:56 +0800

    chore: add judge script

---
+++ run.sh
@@ -0,0 +1,4 @@
+RobotEnv/Robot -m RobotEnv/maps/1.txt "python main.py" -f
+RobotEnv/Robot -m RobotEnv/maps/2.txt "python main.py" -f
+RobotEnv/Robot -m RobotEnv/maps/3.txt "python main.py" -f
+RobotEnv/Robot -m RobotEnv/maps/4.txt "python main.py" -f
